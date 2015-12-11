4eb6cecea9452929c7decd4b88d5821c1c8f6fac
Menno de Jonge <mennodejonge94@gmail.com>
2015-12-11 14:57:45 +0100

    Database connectie toegevoegd
    Database connectie toegevoegd en items ophalen uit database in listview

--- MayaMaya/Form1.Designer.cs
+++ MayaMaya/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listviewItems = new System.Windows.Forms.ListView();
+            this.Naam = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Voorraad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Prijs = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BTW = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listviewItems
+            // 
+            this.listviewItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Naam,
+            this.Voorraad,
+            this.Prijs,
+            this.BTW});
+            this.listviewItems.Enabled = false;
+            this.listviewItems.FullRowSelect = true;
+            this.listviewItems.HoverSelection = true;
+            this.listviewItems.Location = new System.Drawing.Point(12, 12);
+            this.listviewItems.Name = "listviewItems";
+            this.listviewItems.Size = new System.Drawing.Size(644, 464);
+            this.listviewItems.TabIndex = 0;
+            this.listviewItems.UseCompatibleStateImageBehavior = false;
+            this.listviewItems.View = System.Windows.Forms.View.Details;
+            // 
+            // Naam
+            // 
+            this.Naam.Text = "Naam";
+            this.Naam.Width = 400;
+            // 
+            // Voorraad
+            // 
+            this.Voorraad.Text = "Voorraad";
+            this.Voorraad.Width = 63;
+            // 
+            // Prijs
+            // 
+            this.Prijs.Text = "Prijs";
+            // 
+            // BTW
+            // 
+            this.BTW.Text = "BTW heffing";
+            this.BTW.Width = 81;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 488);
+            this.Controls.Add(this.listviewItems);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listviewItems;
+        private System.Windows.Forms.ColumnHeader Naam;
+        private System.Windows.Forms.ColumnHeader Voorraad;
+        private System.Windows.Forms.ColumnHeader Prijs;
+        private System.Windows.Forms.ColumnHeader BTW;
     }
 }
 
--- MayaMaya/Form1.cs
+++ MayaMaya/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace MayaMaya
 {
@@ -15,6 +17,40 @@ namespace MayaMaya
         public Form1()
         {
             InitializeComponent();
+
+            LeesMenu();
+        }
+
+        public void LeesMenu()
+        {
+            //Connect to database
+            string connString = ConfigurationManager
+                .ConnectionStrings["MayaMayaConnectionString"]
+                .ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+
+            //Run SQL command
+            SqlCommand command = new SqlCommand("SELECT * FROM Menu_Items", conn);
+            SqlDataReader reader = command.ExecuteReader();
+
+            //Process results(record for record)
+            while (reader.Read())
+            {
+                //Get values form all the fields
+                string itemNaam = (string)reader["ITEM_NAAM"];
+                int voorraad = (int)reader["VOORRAAD"];
+                float prijs = (float)(double)reader["PRIJS"];
+                int btw = (int)reader["BTW_HEFFING"];
+
+                //Add items to list
+                ListViewItem menuItems = new ListViewItem(itemNaam);
+                menuItems.SubItems.Add(voorraad.ToString());
+                menuItems.SubItems.Add(prijs.ToString());
+                menuItems.SubItems.Add(btw.ToString());
+                listviewItems.Items.Add(menuItems);
+            }
+
         }
     }
 }
