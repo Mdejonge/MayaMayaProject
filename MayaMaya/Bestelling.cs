ac6932943f9daed78811166e05b1d65dabd4b1b3
Menno de Jonge <mennodejonge94@gmail.com>
2016-01-05 17:32:47 +0100

    Methode Database Connection

--- MayaMaya/Bestelling.cs
+++ MayaMaya/Bestelling.cs
@@ -17,14 +17,14 @@ namespace MayaMaya
         {
             InitializeComponent();
 
-            Methodes methode = new Methodes();
+            Bestellingen bestelling = new Bestellingen();
 
             List<int> LunchItemID = new List<int>();
             List<string> LunchItemNaam = new List<string>();
             List<int> LunchItemVoorraad = new List<int>();
             List<float> LunchItemPrijs = new List<float>();
 
-            methode.getLunchItems(out LunchItemID, out LunchItemNaam, out LunchItemVoorraad, out LunchItemPrijs);
+            bestelling.getLunchItems(out LunchItemID, out LunchItemNaam, out LunchItemVoorraad, out LunchItemPrijs);
 
 
             foreach (string value in LunchItemNaam)
@@ -38,6 +38,11 @@ namespace MayaMaya
                     foreach(float prijs in LunchItemPrijs)
                     {
                         Lunch.SubItems.Add(prijs.ToString());
+
+                        foreach(int id in LunchItemID)
+                        {
+                            Lunch.SubItems.Add(id.ToString());
+                        }
                     }
                 }
 
@@ -53,11 +58,41 @@ namespace MayaMaya
 
             foreach(ListViewItem item in Lunch)
             {
+                //Berekent totaalprijs
                 string prijsString = item.SubItems[2].Text.ToString().Trim();
                 float prijsFloat = Single.Parse(prijsString);
                 totaalPrijs = totaalPrijs + prijsFloat;
+
+                //Voegt producten toe aan bestellijst
+                ListViewItem bestellijst = new ListViewItem(item.Text);
+                bestellijst.SubItems.Add(item.SubItems[2]);
+                bestellijst.SubItems.Add(item.SubItems[3]);
+                ListViewBestellijst.Items.Add(bestellijst);
+
+
+                //DIT IS ITEM ID -- item.SubItems[3]
+
+
+
             }
             lbl_TotaalPrijs.Text = Convert.ToString(totaalPrijs);          
+        }
+
+        private void ListViewBestellijst_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in ListViewBestellijst.SelectedItems)
+            {
+                //Verwijderd item
+                ListViewBestellijst.Items.Remove(item);
+
+                //Berekent totaalprijs - van verdwijderd item
+                string prijsString = item.SubItems[1].Text.ToString().Trim();
+                float prijsFloat = Single.Parse(prijsString);
+                totaalPrijs = totaalPrijs - prijsFloat;
+
+                //DIT IS ITEM ID -- item.SubItems[2]
+            }
+            lbl_TotaalPrijs.Text = Convert.ToString(totaalPrijs);
         }
     }
 }
--- MayaMaya/Methodes.cs
+++ MayaMaya/Methodes.cs
@@ -14,36 +14,15 @@ namespace MayaMaya
         public Methodes()
         {
         }
-        public void getLunchItems(out List<int> LunchItemID, out List<string> LunchItemNaam, out List<int> LunchItemVoorraad, out List<float>LunchItemPrijs)
+        
+        public void ConnectDatabase(out SqlConnection conn)
         {
-            LunchItemID = new List<int>();
-            LunchItemNaam = new List<string>();
-            LunchItemVoorraad = new List<int>();
-            LunchItemPrijs = new List<float>();
-
             //Connect to database
             string connString = ConfigurationManager
                 .ConnectionStrings["MayaMayaDatabase"]
                 .ConnectionString;
-            SqlConnection conn = new SqlConnection(connString);
+            conn = new SqlConnection(connString);
             conn.Open();
-
-            //Run SQL command
-            SqlCommand command = new SqlCommand("SELECT DISTINCT Menu_Items.ITEM_ID, Menu_Items.ITEM_NAAM, Menu_Items.VOORRAAD, Menu_Items.PRIJS FROM Menu_Items INNER JOIN Menu_Categorie ON Menu_Items.CATEGORIE_ID = '1' INNER JOIN Menukaart ON Menu_Categorie.KAART_ID = Menukaart.KAART_ID", conn);
-            SqlDataReader reader = command.ExecuteReader();
-
-            //Process results(record for record)
-            while (reader.Read())
-            {
-                //Get values form all the fields
-                int itemID = (int)reader["ITEM_ID"];
-                string itemNaam = (string)reader["ITEM_NAAM"];
-                LunchItemNaam.Add(itemNaam);
-                int voorraad = (int)reader["VOORRAAD"];
-                LunchItemVoorraad.Add(voorraad);
-                float prijs = (float)(double)reader["PRIJS"];
-                LunchItemPrijs.Add(prijs);
-            }
         }
     }
     class Berkeningen
@@ -57,9 +36,9 @@ namespace MayaMaya
             SqlConnection conn = new SqlConnection(connString);
             conn.Open();
 
-            //Run SQL command
-            SqlCommand command = new SqlCommand("", conn);
-            SqlDataReader reader = command.ExecuteReader();
+            ////Run SQL command
+            //SqlCommand command = new SqlCommand("", conn);
+            //SqlDataReader reader = command.ExecuteReader();
 
             if (1 == 1/* checken van wachtwoord via database*/)
             {
